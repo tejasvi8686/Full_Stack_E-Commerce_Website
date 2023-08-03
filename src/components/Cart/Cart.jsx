07cbf4e5f15dde1b1fa93b868c0385fcc093acaf
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-03 19:03:24 +0530

    added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,38 @@
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+
+import CartItem from "./CartItem/CartItem";
+
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+         <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">close</span>
+          </span>
+        </div>
+        
+           {/*<div className="empty-cart">
+            <BsCartX/>
+            <span>No Products in the cart.</span>
+            <button className="return-cta">RETURN TO SHOP</button>
+  </div>*/}
+       
+
+      <>
+      <CartItem/>
+      </>     
+
+
+       
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import "./Header.scss";
 import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
@@ -9,41 +8,45 @@ import Search from "./Search/Search";
 import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 const Header = () => {
-    const [scrolled,setscrolled] = useState(false);
-    const handleScroll = () => {
-        const offset = window.scrollY
-        if(offset > 200 ){
-            setscrolled(true)
-        }  else {
-          setscrolled(false);
-        }
-    };
+  const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+        setScrolled(true);
+    } else {
+        setScrolled(false);
+    }
+};
 
-    useEffect( () => {
-        window.addEventListener("scroll", handleScroll)
-       
-    },[]);
+useEffect(() => {
+  window.addEventListener("scroll", handleScroll);
+}, []);
+
   return (
-    <header className={`main-header ${scrolled ? 'sticky-header':''}`}>
-      <div className="header-content">
-        <ul className="left">
-          <li>Home</li>
-          <li>About</li>
-          <li>Categories</li>
-        </ul>
-        <div className="center">JSDEVSTORE</div>
-        <div className="right">
-        <TbSearch/>
-        <AiOutlineHeart/>
-        <span className="cart-icon">
-          <CgShoppingCart/>
-          <span>
-          5
-          </span>
-        </span>
+    <div>
+    <header
+    className={`main-header ${scrolled ? "sticky-header" : ""}`}
+>
+        <div className="header-content">
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categories</li>
+          </ul>
+          <div className="center">JSDEVSTORE</div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={()=>setShowCart(true)}>
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    {showCart && <Cart setShowCart={setShowCart}/>}
+    </div>
   );
 };
 
