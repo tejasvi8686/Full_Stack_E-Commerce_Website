0a537ead38c83fd73f7afceb76ef5891612db50a
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-03 23:18:37 +0530

    added search product model

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+//import {useContext} from "react";
+//import { useNavigate } from "react-router-dom";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
 import "./Header.scss";
 import Search from "./Search/Search";
-import { Context } from "../../utils/context";
+//import { Context } from "../../utils/context";
 import Cart from "../Cart/Cart";
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
   const [showCart, setShowCart] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   const handleScroll = () => {
     const offset = window.scrollY;
     if (offset > 200) {
@@ -36,7 +38,7 @@ useEffect(() => {
           </ul>
           <div className="center">JSDEVSTORE</div>
           <div className="right">
-            <TbSearch />
+            <TbSearch onClick={()=>setShowSearch(true)} />
             <AiOutlineHeart />
             <span className="cart-icon" onClick={()=>setShowCart(true)}>
               <CgShoppingCart />
@@ -46,6 +48,7 @@ useEffect(() => {
         </div>
       </header>
     {showCart && <Cart setShowCart={setShowCart}/>}
+   {showSearch && <Search setShowSearch={setShowSearch}/>}
     </div>
   );
 };
