346babfca013aeb07579a6bd9b9458183f46373c
tejasvi8686 <tejasviraj8686@gmail.com>
2023-08-05 09:29:17 +0530

    added new branch

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./Category.scss";
 
 const Category = ({ categories }) => {
-  const navigate = useNavigate();
+
   
   return (
     <div className="shop-by-category">
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,8 +5,8 @@ const Products = ({ products, innerPage, headingText }) => {
     return (
         <div className="products-container">
             {!innerPage && <div className="sec-heading">{headingText}</div>}
-            <div className="products">
-                {products.data.map((item) => (
+            <div className={`products ${innerPage ? "innerPage" : ""}`}>
+                {products?.data?.map((item) => (
                     <Product
                         key={item.id}
                         id={item.id}
